7f2c4910208b46fa83ae626958cc676a222e5908
na2kera <nat3mitaro@gmail.com>
2024-01-09 21:39:27 +0900

    マップにページ移動するボタンを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
+import Link from "next/link";
 import Post from "./components/Post";
+require("dotenv").config();
 
 export default function Home() {
   return (
     <>
+      <Link href="/map">
+        <button
+          className="w-1/10 py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          type="button"
+        >
+          地図を表示
+        </button>
+      </Link>
+
       <Post />
     </>
   );
