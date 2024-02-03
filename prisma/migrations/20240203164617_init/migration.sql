1d0a0b029e8dbe501efaea512a6e80194d85b429
na2kera <nat3mitaro@gmail.com>
2024-02-04 02:02:47 +0900

    名前の変更とマイグレーション

---
+++ prisma/migrations/20240203164617_init/migration.sql
@@ -0,0 +1,21 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "Post";
+
+-- CreateTable
+CREATE TABLE "Data" (
+    "id" SERIAL NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "user" TEXT NOT NULL,
+    "detail" TEXT NOT NULL,
+    "roomKey" TEXT NOT NULL,
+    "latitude" TEXT NOT NULL,
+    "longitude" TEXT NOT NULL,
+
+    CONSTRAINT "Data_pkey" PRIMARY KEY ("id")
+);
