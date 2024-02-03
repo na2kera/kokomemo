ced55e84d79c8e26896d2ab69c159c142091a67d
na2kera <nat3mitaro@gmail.com>
2024-02-04 02:04:10 +0900

    GETメソッドを追加

---
+++ src/app/api/list/[roomKey]/route.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+import { main, prisma } from "../route";
+
+export const GET = async (req: Request, res: NextResponse) => {
+  try {
+    const roomKey: string = req.url.split("/map/")[1];
+    await main();
+    const locationData = await prisma.data.findMany({
+      where: { roomKey: roomKey },
+    });
+    return NextResponse.json(
+      { message: "Success", locationData },
+      { status: 200 }
+    );
+  } catch (err) {
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
