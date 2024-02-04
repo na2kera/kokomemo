00adf233c8d5d9340b2d8b7e0b7dae6d7cbe7a8f
na2kera <nat3mitaro@gmail.com>
2024-02-04 16:15:42 +0900

    フェッチする関数

---
+++ src/app/map/[roomKey]/getLocationData.ts
@@ -0,0 +1,7 @@
+async function getLocationData(roomKey: string) {
+  const res = await fetch(`http://localhost:3000/api/list/{roomKey}`, {
+    cache: "no-store",
+  });
+  const data = await res.json();
+  return data.locationData;
+}
