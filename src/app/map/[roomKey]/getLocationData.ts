063089f8233a475d3e4cda5e65f71920bf966a56
na2kera <nat3mitaro@gmail.com>
2024-02-05 20:55:11 +0900

    export dafaultを追加

--- src/app/map/[roomKey]/getLocationData.ts
+++ src/app/map/[roomKey]/getLocationData.ts
@@ -1,7 +1,9 @@
 async function getLocationData(roomKey: string) {
-  const res = await fetch(`http://localhost:3000/api/list/{roomKey}`, {
+  const res = await fetch(`http://localhost:3000/api/list/${roomKey}`, {
     cache: "no-store",
   });
   const data = await res.json();
   return data.locationData;
 }
+
+export default getLocationData;
