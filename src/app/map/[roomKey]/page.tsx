f707189f26f9972082bfd20e77f902ecb5c2eb82
na2kera <nat3mitaro@gmail.com>
2024-02-06 15:31:23 +0900

    戻るボタン、現在位置に戻るボタンを実装

--- src/app/map/[roomKey]/page.tsx
+++ src/app/map/[roomKey]/page.tsx
@@ -7,8 +7,11 @@ import {
 } from "@react-google-maps/api";
 import { useEffect, useState } from "react";
 import getLocationData from "./getLocationData";
+import Link from "next/link";
 
 const map = ({ params }: { params: { roomKey: string } }) => {
+  const [latitudeNow, setLatitudeNow] = useState(34.6133956);
+  const [longitudeNow, setLongitudeNow] = useState(140.1015564);
   const [locationData, setLocationData] = useState([]);
 
   useEffect(() => {
@@ -23,7 +26,33 @@ const map = ({ params }: { params: { roomKey: string } }) => {
     width: "100%",
     height: "100vh",
   };
-  const position = { lat: 35.6133956, lng: 140.1015564 };
+  const position = { lat: latitudeNow, lng: longitudeNow };
+
+  const getLocationNow = () => {
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 10000,
+      maximumAge: 0,
+    };
+
+    function success(pos: any) {
+      const crd = pos.coords;
+
+      console.log("Your current position is:");
+      console.log(`Latitude : ${crd.latitude}`);
+      console.log(`Longitude: ${crd.longitude}`);
+      console.log(`More or less ${crd.accuracy} meters.`);
+
+      setLatitudeNow(crd.latitude);
+      setLongitudeNow(crd.longitude);
+    }
+
+    function error(err: any) {
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+
+    navigator.geolocation.getCurrentPosition(success, error, options);
+  };
 
   return (
     <>
@@ -31,7 +60,7 @@ const map = ({ params }: { params: { roomKey: string } }) => {
         <LoadScript
           googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY || ""}
         >
-          <GoogleMap mapContainerStyle={container} center={position} zoom={15}>
+          <GoogleMap mapContainerStyle={container} center={position} zoom={20}>
             {locationData.map((data: Data) => (
               <MarkerF
                 key={data.id}
@@ -39,11 +68,26 @@ const map = ({ params }: { params: { roomKey: string } }) => {
                   lat: parseFloat(data.latitude),
                   lng: parseFloat(data.longitude),
                 }}
-                label={data.detail}
+                label={data.user}
               />
             ))}
           </GoogleMap>
         </LoadScript>
+        <Link href="/" className="fixed left-0 bottom-0">
+          <button
+            className="w-1/10 py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            type="button"
+          >
+            戻る
+          </button>
+        </Link>
+        <button
+          className="fixed right-0 bottom-0 w-1/10 py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          type="button"
+          onClick={getLocationNow}
+        >
+          現在位置
+        </button>
       </div>
     </>
   );
