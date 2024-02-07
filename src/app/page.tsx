9e65cb70a386d73343e096fc480f76d8c3fdf0b0
na2kera <nat3mitaro@gmail.com>
2024-02-07 21:38:56 +0900

    コンポーネント化

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,11 @@
-import Link from "next/link";
 import Send from "./components/Send";
+import Room from "./components/Room";
 require("dotenv").config();
 
 export default function Home() {
   return (
     <>
-      <Link href="/map">
-        <button
-          className="w-1/10 py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          type="button"
-        >
-          地図を表示
-        </button>
-      </Link>
-
+      <Room />
       <Send />
     </>
   );
