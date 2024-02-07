dc1f1761b92b75bd719a88e53ba93ea8924f3f20
na2kera <nat3mitaro@gmail.com>
2024-02-07 21:39:11 +0900

    コンポーネント化

---
+++ src/app/components/Room.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import React from "react";
+
+const Room = () => {
+  return (
+    <Link href="/map">
+      <button
+        className="w-1/10 py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        type="button"
+      >
+        地図を表示
+      </button>
+    </Link>
+  );
+};
+
+export default Room;
