f23bace2a362950c4e70ef9538a7cc6939aef3cf
na2kera <nat3mitaro@gmail.com>
2024-02-09 14:55:55 +0900

    cssを調整

--- src/app/components/Room.tsx
+++ src/app/components/Room.tsx
@@ -14,22 +14,23 @@ const Room = () => {
   return (
     <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
       <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="roomName">ルーム名</label>
+        <div className="flex justify-center items-center">
           <input
             id="roomName"
             name="roomName"
             ref={roomNameRef}
             type="text"
             required
+            className="mt-1 block  border border-gray-300 rounded-md shadow-sm py-2 px-3 text-base focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+            placeholder="ルーム名を入力"
           />
+          <button
+            className="w-1/10 py-3 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            type="submit"
+          >
+            地図を表示
+          </button>
         </div>
-        <button
-          className="w-1/10 py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          type="submit"
-        >
-          地図を表示
-        </button>
       </form>
     </div>
   );
--- src/app/components/Send.tsx
+++ src/app/components/Send.tsx
@@ -74,8 +74,8 @@ const Send = () => {
           メモを作成
         </h2>
       </div>
-      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+      <div className=" sm:mx-auto sm:w-full sm:max-w-md">
+        <div className="bg-white pb-20 px-4 shadow sm:rounded-lg sm:px-10">
           <form onSubmit={handleSubmit}>
             <div>
               <label
@@ -132,10 +132,10 @@ const Send = () => {
                 }
               />
             </div>
-            <div className="mt-6">
+            <div className="mt-6 flex justify-center">
               <button
                 type="button"
-                className="w-1/10 py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                className="py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 onClick={getLocation}
               >
                 現在地を所得
