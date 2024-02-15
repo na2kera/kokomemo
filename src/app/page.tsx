8dc67b18e1597c3fff781e5c573b5ff93cb672ea
na2kera <nat3mitaro@gmail.com>
2024-02-15 23:27:45 +0900

    clerk配置

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Send from "./components/Send";
 import Room from "./components/Room";
+import { UserButton } from "@clerk/nextjs";
 require("dotenv").config();
 
 export default function Home() {
   return (
     <>
       <Room />
+      <UserButton afterSignOutUrl="/" />
       <Send />
     </>
   );
