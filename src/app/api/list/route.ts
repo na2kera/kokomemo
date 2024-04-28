f5de90aa88c76ae13e4df08d05846ae9dd014f56
na2kera <nat3mitaro@gmail.com>
2024-04-28 13:13:55 +0900

    不要なインポートを削除

--- src/app/api/list/route.ts
+++ src/app/api/list/route.ts
@@ -1,5 +1,3 @@
-import { Prisma, PrismaClient } from "@prisma/client";
-import { NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server";
 import prisma from "./prisma";
 
